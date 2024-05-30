db1f2d027eeb4f67d09653efa8cc830f0e2167f3
Mikhail Sirkin <sirkin@algont.ru>
2024-05-30 19:31:06 +0300

    codestyle

--- internal/mem_storage.go
+++ internal/mem_storage.go
@@ -1,21 +1,21 @@
 package internal
 
 type MemStorage struct {
-	gauge   map[string]float64
-	counter map[string]int64
+	Gauge   map[string]float64
+	Counter map[string]int64
 }
 
 func NewMemStorage() *MemStorage {
 	var m MemStorage
-	m.gauge = make(map[string]float64)
-	m.counter = make(map[string]int64)
+	m.Gauge = make(map[string]float64)
+	m.Counter = make(map[string]int64)
 	return &m
 }
 
 func (m *MemStorage) UpdateGauge(key string, value float64) {
-	m.gauge[key] = value
+	m.Gauge[key] = value
 }
 
 func (m *MemStorage) UpdateCounter(key string, value int64) {
-	m.counter[key] += value
+	m.Counter[key] += value
 }
