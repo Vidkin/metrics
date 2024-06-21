17b43acca4f3131e823312af1b4968a0dc46d91b
Mikhail Sirkin <sirkin@algont.ru>
2024-06-21 09:09:25 +0300

    Реализован handler для обработки json-запросов обновления метрик

--- internal/domain/handlers/metric_router.go
+++ internal/domain/handlers/metric_router.go
@@ -1,9 +1,12 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/Vidkin/metrics/internal/logger"
+	"github.com/Vidkin/metrics/internal/models"
 	"github.com/go-chi/chi/v5"
+	"go.uber.org/zap"
 	"io"
 	"net/http"
 	"strconv"
@@ -43,6 +46,7 @@ func NewMetricRouter(repository Repository) *MetricRouter {
 			r.Get("/{metricType}/{metricName}", logger.LoggingHandler(mr.GetMetricValueHandler))
 		})
 		router.Route("/update", func(r chi.Router) {
+			r.Post("/", logger.LoggingHandler(mr.UpdateMetricHandlerJSON))
 			r.Post("/{metricType}/{metricName}/{metricValue}", logger.LoggingHandler(mr.UpdateMetricHandler))
 		})
 	})
@@ -78,6 +82,7 @@ func (mr *MetricRouter) GetMetricValueHandler(res http.ResponseWriter, req *http
 			valueAsString := strconv.FormatFloat(metricValue, 'g', -1, 64)
 			_, err := res.Write([]byte(valueAsString))
 			if err != nil {
+				logger.Log.Info("can't convert metric value", zap.Error(err))
 				http.Error(res, "Can't convert metric value", http.StatusInternalServerError)
 			}
 		} else {
@@ -88,6 +93,7 @@ func (mr *MetricRouter) GetMetricValueHandler(res http.ResponseWriter, req *http
 			valueAsString := strconv.FormatInt(metricValue, 10)
 			_, err := res.Write([]byte(valueAsString))
 			if err != nil {
+				logger.Log.Info("can't convert metric value", zap.Error(err))
 				http.Error(res, "Can't convert metric value", http.StatusInternalServerError)
 			}
 		} else {
@@ -112,12 +118,14 @@ func (mr *MetricRouter) UpdateMetricHandler(res http.ResponseWriter, req *http.R
 	switch metricType {
 	case MetricTypeGauge:
 		if value, err := strconv.ParseFloat(metricValue, 64); err != nil {
+			logger.Log.Info("can't convert metric value", zap.Error(err))
 			http.Error(res, "Bad metric value!", http.StatusBadRequest)
 		} else {
 			mr.Repository.UpdateGauge(metricName, value)
 		}
 	case MetricTypeCounter:
 		if value, err := strconv.ParseInt(metricValue, 10, 64); err != nil {
+			logger.Log.Info("can't convert metric value", zap.Error(err))
 			http.Error(res, "Bad metric value!", http.StatusBadRequest)
 		} else {
 			mr.Repository.UpdateCounter(metricName, value)
@@ -126,5 +134,69 @@ func (mr *MetricRouter) UpdateMetricHandler(res http.ResponseWriter, req *http.R
 		http.Error(res, "Bad metric type!", http.StatusBadRequest)
 	}
 	res.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	res.WriteHeader(http.StatusOK)
+}
+
+func (mr *MetricRouter) UpdateMetricHandlerJSON(res http.ResponseWriter, req *http.Request) {
+	if req.Header.Get("Content-Type") != "application/json" {
+		logger.Log.Info(
+			"content-type is not allowed",
+			zap.String("content-type", req.Header.Get("Content-Type")))
+		http.Error(res, "only application/json content-type allowed", http.StatusBadRequest)
+		return
+	}
+
+	var metrics []models.Metrics
+	dec := json.NewDecoder(req.Body)
+	if err := dec.Decode(&metrics); err != nil {
+		logger.Log.Info("can't decode request body", zap.Error(err))
+		http.Error(res, "can't decode request body", http.StatusBadRequest)
+		return
+	}
+
+	for _, metric := range metrics {
+		if metric.MType == MetricTypeGauge {
+			if metric.Value == nil {
+				continue
+			}
+			mr.Repository.UpdateGauge(metric.ID, *metric.Value)
+
+		}
+		if metric.MType == MetricTypeCounter {
+			if metric.Delta == nil {
+				continue
+			}
+			mr.Repository.UpdateCounter(metric.ID, *metric.Delta)
+		}
+	}
+
+	gauges := mr.Repository.GetGauges()
+	counters := mr.Repository.GetCounters()
+	respMetrics := make([]models.Metrics, 0, len(gauges)+len(counters))
+
+	for k, v := range gauges {
+		respMetrics = append(respMetrics, models.Metrics{
+			ID:    k,
+			MType: MetricTypeGauge,
+			Value: &v,
+		})
+	}
+
+	for k, v := range counters {
+		respMetrics = append(respMetrics, models.Metrics{
+			ID:    k,
+			MType: MetricTypeCounter,
+			Delta: &v,
+		})
+	}
+
+	res.Header().Set("Content-Type", "application/json")
+	enc := json.NewEncoder(res)
+
+	if err := enc.Encode(respMetrics); err != nil {
+		logger.Log.Info("error encoding response", zap.Error(err))
+		http.Error(res, "error encoding response", http.StatusInternalServerError)
+		return
+	}
 	res.WriteHeader(http.StatusOK)
 }
