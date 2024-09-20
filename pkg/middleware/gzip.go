4139ac5111135ee9b2f37424012c22e247f6bf17
Mikhail Sirkin <sirkin@algont.ru>
2024-09-20 09:15:53 +0300

    Реализовано закрытие req.body там, где оно отсутствовало

--- pkg/middleware/gzip.go
+++ pkg/middleware/gzip.go
@@ -11,16 +11,25 @@ import (
 func Gzip(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ow := w
-
 		acceptEncoding := r.Header.Get("Accept-Encoding")
 		if strings.Contains(acceptEncoding, "gzip") {
 			cw := compress.NewCompressWriter(w)
 			ow = cw
-			defer cw.Close()
+			defer func() {
+				if err := cw.Close(); err != nil {
+					logger.Log.Error("error close compress writer", zap.Error(err))
+				}
+			}()
 		}
 
 		contentEncoding := r.Header.Get("Content-Encoding")
 		if strings.Contains(contentEncoding, "gzip") {
+			defer func() {
+				if err := r.Body.Close(); err != nil {
+					logger.Log.Error("error close body", zap.Error(err))
+				}
+			}()
+
 			cr, err := compress.NewCompressReader(r.Body)
 			if err != nil {
 				logger.Log.Error("error init compress reader", zap.Error(err))
@@ -28,7 +37,11 @@ func Gzip(h http.Handler) http.Handler {
 				return
 			}
 			r.Body = cr
-			defer cr.Close()
+			defer func() {
+				if err := cr.Close(); err != nil {
+					logger.Log.Error("error close compress reader", zap.Error(err))
+				}
+			}()
 		}
 
 		h.ServeHTTP(ow, r)
