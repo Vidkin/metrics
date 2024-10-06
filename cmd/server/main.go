0ba1ddd272879b59cde5b31856c1cf9404567b15
Mikhail Sirkin <sirkin@algont.ru>
2024-10-06 23:37:45 +0300

    Добавлен вывод информации о билде

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/Vidkin/metrics/app"
 	"github.com/Vidkin/metrics/internal/config"
 )
 
+var (
+	buildVersion = "N/A"
+	buildDate    = "N/A"
+	buildCommit  = "N/A"
+)
+
 func main() {
+	fmt.Printf("Build version: %s\nBuild date: %s\nBuild commit: %s\n", buildVersion, buildDate, buildCommit)
 	cfg, err := config.NewServerConfig()
 	if err != nil {
 		panic(err)
